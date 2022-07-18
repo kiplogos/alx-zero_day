dd2b5133677694764b285f185bbc4c3c09f6947f
kiplogos <logosk98@gmail.com>
2022-07-18 12:18:05 -0700

    q0

---
+++ root/alx-low_level_programming/0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print a string
+ *
+ * Description: use printf
+ *
+ * Return: ok
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
---
+++ root/alx-low_level_programming/0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - concatenates n bytes from a string to another
+ *
+ * @dest:destination string
+ * @src:source string 
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0;
+	int y = 0;
+
+	while (dest[x])
+	{
+		x++;
+	}
+	while (y < n && src[y] != 0)
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	dest[x] = 0;
+	return (dest);
+}
